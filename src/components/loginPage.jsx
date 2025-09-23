b7eb61072c225ec070705809f159f3f6b6d7b0ab
Nhutnguyen395 <nhutnguyen395@gmail.com>
2025-09-22 23:40:48 -0700

    .

--- src/components/loginPage.jsx
+++ src/components/loginPage.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import { login } from '../services/authService.js';
+import { login } from '../../services/authService.js';
 
 function loginPage() {
   /*
